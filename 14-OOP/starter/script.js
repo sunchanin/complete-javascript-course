8c61ad9db717000bc2724212f493b1e887320cc2
chanin <136733812+sunchanin@users.noreply.github.com>
2024-09-07 19:58:51 +0700

    add oop

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -15,6 +15,75 @@ Person.prototype.calcAge = function () {
   console.log(2024 - this.birthYear);
 };
 
+// Class declaration
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+}
+
+const sun = new PersonCl('Chanin', 2000);
+
+// Setters and Getters
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+account.latest = 600;
+
+// Inheritance Between Classes: Constructor Function
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study in ${this.course}`);
+};
+
+const mike = new Student('Mike', 2000, 'Computer Science');
+
+// Inheritance Between Classes: ES6 Classes
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always need to happen first!!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study in ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(`I'm ${2024 - this.birthYear} years old`);
+  }
+}
+
+const sunChanin = new StudentCl('Chanin Krewkerdmee', 2000, 'Computer Science');
+sunChanin.calcAge();
+sunChanin.introduce();
+
 ///////////////////////////////////////
 // Coding Challenge #1
 
@@ -49,36 +118,71 @@ const BMW = new Car(null, 120);
 const Mercedes = new Car(null, 95);
 
 ///////////////////////////////////////
+// Coding Challenge #2
 
-// Class declaration
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
 
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
   }
 
-  calcAge() {
-    console.log(2024 - this.birthYear);
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
   }
 }
 
-const sun = new PersonCl('Chanin', 2000);
+const Ford = new CarCl('Ford', 120);
 
-// Setters and Getters
+///////////////////////////////////////
 
-const account = {
-  owner: 'Jonas',
-  movements: [200, 530, 120, 300],
+// Coding Challenge #3
 
-  get latest() {
-    return this.movements.slice(-1).pop();
-  },
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
 
-  set latest(mov) {
-    this.movements.push(mov);
-  },
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+
+GOOD LUCK 😀
+*/
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
 };
 
-account.latest = 600;
-console.log(account.latest);
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const Tesla = new EV('Tesla', 120, 23);
+
+///////////////////////////////////////
